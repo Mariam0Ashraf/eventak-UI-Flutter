707b24c2d272fa8096654a48e6128fe5a94bef5d
MariamESiddek <mariam47emad@gmail.com>
2025-11-21 03:06:12 +0200

    UI updates in HomeView and profile navigation

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,41 @@ import 'package:eventak/features/auth/view/reset_passwrd_view.dart';
 import 'package:eventak/splash.dart';
 import 'package:flutter/material.dart';
 
+import 'package:eventak/core/constants/app-colors.dart';
+import 'package:eventak/features/home/home_view.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Eventak',
-      
-      home: UserProfilePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColor.primary,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: AppColor.primary,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: AppColor.blueFont),
+          titleLarge: TextStyle(
+            color: AppColor.blueFont,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: AppColor.secondaryBlue,
+        ),
+      ),
+      home: const HomeView(),
+      // you can change home to UserProfilePage when needed
     );
   }
 }
